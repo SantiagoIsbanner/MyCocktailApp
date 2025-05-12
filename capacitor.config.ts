1d4df2bccf7c8a3349a997a62917696433cc1b6f
Nova <sni15396@gmail.com>
2025-05-12 08:40:32 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'myCocktailApp',
+  webDir: 'www'
+};
+
+export default config;
