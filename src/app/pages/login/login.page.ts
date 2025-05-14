63402a13dbb7899a5c753bfa4eb4d6e93517a6eb
sanntzherrera1 <luis.herreraipc@gmail.com>
2025-05-14 07:33:17 -0300

    feat(auth): add toast notifications for login success and error

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 import { FormControl, FormGroup, FormsModule, Validators } from '@angular/forms';
-
+import { ToastController } from '@ionic/angular';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -12,7 +12,11 @@ import { FormControl, FormGroup, FormsModule, Validators } from '@angular/forms'
 export class LoginPage {
   loginForm: FormGroup;
 
-  constructor(private authService: AuthService, private router: Router) {
+  constructor(
+    private authService: AuthService, 
+    private router: Router,
+    private toastController: ToastController
+  ) {
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required, Validators.minLength(6)])
@@ -23,21 +27,49 @@ export class LoginPage {
     const { email, password } = this.loginForm.value;
     
     this.authService.login(email, password)
-      .then(() => this.router.navigate(['/home']))
-      .catch(error => alert("Usuario o contraseña invalidos: " + error.message));
+      .then(() => {
+        this.toastWelcome('top');//muestra el toast de bienvenida
+        this.router.navigate(['/home']);
+      })
+      .catch(error => this.toastError('top'));//muestra el toast de error
   }
 
   register() {
     const { email, password } = this.loginForm.value;
     
     this.authService.register(email, password)
-      .then(() => alert('Registro exitoso. Ahora puedes iniciar sesión.'))
-      .catch(error => alert("Error al registrarse: " + error.message));
+      .then(() => {this.toastWelcome('top');
+      })  
+  
+      .catch(error => this.toastError('top'));
   }
 
   loginGoogle() {
     this.authService.loginWithGoogle()
-      .then(() => this.router.navigate(['/home']))
-      .catch(error => alert("Error en login con Google: " + error.message));
+      .then(() => {
+        this.toastWelcome('top');
+        this.router.navigate(['/home']);
+      })
+      .catch(error => this.toastError('top'));
+    }
+
+toastError(position: 'top' | 'middle' | 'bottom') {//Toast para mensaje de error
+  this.toastController.create({
+    message: 'Usuario o contraseña invalidos',
+    duration: 2500,
+    position: position
+    }).then((toast: any) => toast.present());
+  }
+
+  toastWelcome(position: 'top' | 'middle' | 'bottom') {//Toast para mensaje de bienvenida
+    this.toastController.create({
+      message: `Bienvenido ${this.loginForm.value.email} a la app de cocktails`,//evalua el email del usuario y lo usa para mostrar el msj de bienvenida con ese mail
+      duration: 2500,
+      position: position
+      }).then((toast: any) => toast.present());
+    }
+
+
+   
 }
-}
+
