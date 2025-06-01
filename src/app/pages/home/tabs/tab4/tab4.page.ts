cdb769c12ac44740acfa82c77f2456b5b8624d9f
sanntzherrera1 <luis.herreraipc@gmail.com>
2025-06-01 13:58:22 -0300

    feat(perfil): agregué la sección con foto, usuario, email y el botón de cerrar sesión

--- src/app/pages/home/tabs/tab4/tab4.page.ts
+++ src/app/pages/home/tabs/tab4/tab4.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';/*importo authservice para verificar data del user*/
 
 @Component({
   standalone: false,
@@ -7,10 +9,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tab4.page.scss']
 })
 export class Tab4Page implements OnInit {
+  
+  userData: any = {};
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private authService: AuthService
+  ) { 
 
-  ngOnInit() {
+   this.authService.getCurrentUser().then(user => {/*trae la data del user actual */
+     this.userData = user;
+     /*console.log(user);muestra la data del user actual en consola*/
+   });
+
   }
 
+  ngOnInit() {
+    
+  }
+
+  // Método para generar nombre de usuario a partir del email
+  getUsernameFromEmail(): string {
+    if (!this.userData?.email) return 'Usuario';
+    
+    // Extrae la parte antes del @ y reemplaza puntos por espacios
+    return this.userData.email.split('@')[0]
+      .replace(/\./g, ' ')
+      .replace(/\b\w/g, (l: string) => l.toUpperCase()); // Capitaliza cada palabra
+  }
+
+  logout() {/*cierre de sesion*/
+    this.authService.logout()
+    .then(()=>this.router.navigate(['/login']))
+    .catch(error=>alert("Error al cerrar sesion: "+error.message))
+    }
+    
+  
+
+  
 }
